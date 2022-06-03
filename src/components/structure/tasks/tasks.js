c4b0a6ad0d2eaa5afcc308d1664b0cdd0c87c509
Patrick Hladun <patrickhladun@gmail.com>
2022-06-03 13:13:35 +0100

    Add basic structure of the tasks list

--- src/components/structure/tasks/tasks.js
+++ src/components/structure/tasks/tasks.js
@@ -10,6 +10,15 @@ class Tasks extends React.Component {
             <Fragment>
               <div className="app-tasks-main">
                 <span>Tasks</span>
+                
+                <div className="app-tasks-list">
+                  <div className="app-task-item">Task number 1</div>
+                  <div className="app-task-item">Task number 2</div>
+                  <div className="app-task-item">Task number 3</div>
+                </div>
+
+                <button>Add Task</button>
+                
               </div>
             </Fragment>
         )
